3998f685f7920145f8ae8b800a17b8d59f7c6455
Dominik Stodolny <dominik@stodolny.org>
2020-05-03 13:53:31 +0100

    solve translate_reg

--- proj1/src/translate_utils.c
+++ proj1/src/translate_utils.c
@@ -38,22 +38,22 @@ int is_valid_label(const char* str) {
     return first ? 0 : 1;   // empty string is invalid
 }
 
-/* Translate the input string into a signed number. The number is then 
+/* Translate the input string into a signed number. The number is then
    checked to be within the correct range (note bounds are INCLUSIVE)
-   ie. NUM is valid if LOWER_BOUND <= NUM <= UPPER_BOUND. 
+   ie. NUM is valid if LOWER_BOUND <= NUM <= UPPER_BOUND.
 
    The input may be in either positive or negative, and be in either
    decimal or hexadecimal format. It is also possible that the input is not
-   a valid number. Fortunately, the library function strtol() can take 
+   a valid number. Fortunately, the library function strtol() can take
    care of all that (with a little bit of work from your side of course).
    Please read the documentation for strtol() carefully. Do not use strtoul()
-   or any other variants. 
+   or any other variants.
 
-   You should store the result into the location that OUTPUT points to. The 
-   function returns 0 if the conversion proceeded without errors, or -1 if an 
+   You should store the result into the location that OUTPUT points to. The
+   function returns 0 if the conversion proceeded without errors, or -1 if an
    error occurred.
  */
-int translate_num(long int* output, const char* str, long int lower_bound, 
+int translate_num(long int* output, const char* str, long int lower_bound,
     long int upper_bound) {
     if (!str || !output) {
         return -1;
@@ -74,6 +74,18 @@ int translate_reg(const char* str) {
     else if (strcmp(str, "$at") == 0)   return 1;
     else if (strcmp(str, "$v0") == 0)   return 2;
     else if (strcmp(str, "$a0") == 0)   return 4;
-    /* YOUR CODE HERE */
-    else                                return 0;
+    else if (strcmp(str, "$a1") == 0)   return 5;
+    else if (strcmp(str, "$a2") == 0)   return 6;
+    else if (strcmp(str, "$a3") == 0)   return 7;
+    else if (strcmp(str, "$t0") == 0)   return 8;
+    else if (strcmp(str, "$t1") == 0)   return 9;
+    else if (strcmp(str, "$t2") == 0)   return 10;
+    else if (strcmp(str, "$t3") == 0)   return 11;
+    else if (strcmp(str, "$s0") == 0)   return 16;
+    else if (strcmp(str, "$s1") == 0)   return 17;
+    else if (strcmp(str, "$s2") == 0)   return 18;
+    else if (strcmp(str, "$s3") == 0)   return 19;
+    else if (strcmp(str, "$sp") == 0)   return 29;
+    else if (strcmp(str, "$ra") == 0)   return 31;
+    else                                return -1;
 }
