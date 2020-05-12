cac83a6d832169e6b7315c1424c6c1e8b7e4b944
Dominik Stodolny <dominik@stodolny.org>
2020-05-12 21:16:55 +0100

    implement addr_for_symbol and add_to_list

--- proj1/linker-src/symbol_list.s
+++ proj1/linker-src/symbol_list.s
@@ -1,4 +1,4 @@
-# CS 61C Spring 2015 Project 1-2 
+# CS 61C Spring 2015 Project 1-2
 # symbol_list.s
 
 #==============================================================================
@@ -11,7 +11,7 @@
 #
 # Each node in the SymbolList contains a (addr, name) pair. An empty SymbolList
 # is simply a pointer to NULL. As (addr, name) pairs are added to a SymbolList,
-# new list nodes are dynamically allocated and appended to the front of the list. 
+# new list nodes are dynamically allocated and appended to the front of the list.
 # However, as there is no free() in MARS, you do not need to write a free_list()
 # function. You may use the functions in string.s during your implementation.
 #
@@ -19,12 +19,12 @@
 # in SymbolList, nor do you need to do alignment checking. Don't worry about
 # provided addresses being too big.
 #
-# If SymbolList were to be written in C, each list node would be declared as 
+# If SymbolList were to be written in C, each list node would be declared as
 # follows:
-#   typedef struct symbollist { 
+#   typedef struct symbollist {
 #       int addr;
 #       char* name;
-#       struct symbollist* next; 
+#       struct symbollist* next;
 #   } SymbolList;
 #
 # You need to write addr_for_symbol() and add_to_list().
@@ -48,14 +48,37 @@
 # Returns:  address of symbol if found or -1 if not found
 #------------------------------------------------------------------------------
 addr_for_symbol:
-	# YOUR CODE HERE
-	jr $ra
-	
+        addiu $sp, $sp, -12
+        sw $ra, 0($sp)
+        sw $a0, 4($sp)
+        sw $a1, 8($sp)
+addr_for_symbol_loop:
+        beq $a0, $0, addr_not_found
+        lw $a0, 4($a0)
+        jal streq
+        lw $a0, 4($sp)
+        lw $a1, 8($sp)
+        beq $v0, $0, addr_found
+        lw $a0, 8($a0)
+        jal addr_for_symbol
+        j addr_for_symbol_done
+addr_found:
+        lw $v0, 0($a0)
+        j addr_for_symbol_done
+addr_not_found:
+        li $v0, -1
+addr_for_symbol_done:
+        lw $a1, 8($sp)
+        lw $a0, 4($sp)
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 12
+        jr $ra
+
 #------------------------------------------------------------------------------
 # function add_to_list()
 #------------------------------------------------------------------------------
 # Adds a (name, addr) pair to the FRONT of the list. You should call new_node()
-# to create a new node. You do not need to perform error checking on duplicate 
+# to create a new node. You do not need to perform error checking on duplicate
 # addr or name entries in SymbolList, nor do you need to do alignment checking.
 #
 # As with Project 1-1, you WILL need to create a copy of the string that was
@@ -69,12 +92,24 @@ addr_for_symbol:
 #
 # Returns: the new list
 #------------------------------------------------------------------------------
-add_to_list:	
-	# YOUR CODE HERE
-	jr $ra
+add_to_list:
+        addiu $sp, $sp, -8
+        sw $ra, 0($sp)
+        sw $a0, 4($sp)
+
+        jal new_node
+        lw $a0, 4($sp)
+        sw $a1, 0($v0)
+        sw $a2, 4($v0)
+        sw $a0, 8($v0)
+
+        lw $a0, 4($sp)
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 8
+        jr $ra
 
 ###############################################################################
-#                 DO NOT MODIFY ANYTHING BELOW THIS POINT                       
+#                 DO NOT MODIFY ANYTHING BELOW THIS POINT
 ###############################################################################
 
 #------------------------------------------------------------------------------
@@ -90,17 +125,17 @@ add_to_list:
 # Returns: a pointer to the name if found or NULL if not found
 #------------------------------------------------------------------------------
 symbol_for_addr:
-	beq $a0, $0, symbol_not_found	# Begin symbol_for_addr
-	lw $t0, 0($a0)
-	beq $t0, $a1, symbol_found
-	lw $a0, 8($a0)
-	j symbol_for_addr
+        beq $a0, $0, symbol_not_found	# Begin symbol_for_addr
+        lw $t0, 0($a0)
+        beq $t0, $a1, symbol_found
+        lw $a0, 8($a0)
+        j symbol_for_addr
 symbol_found:
-	lw $v0, 4($a0)
-	jr $ra
+        lw $v0, 4($a0)
+        jr $ra
 symbol_not_found:
-	li $v0, 0
-	jr $ra			# End addr_for_symbol
+        li $v0, 0
+        jr $ra			# End addr_for_symbol
 
 #------------------------------------------------------------------------------
 # function print_list() - DO NOT MODIFY THIS FUNCTION
@@ -111,29 +146,29 @@ symbol_not_found:
 #  $a2 = file pointer
 #------------------------------------------------------------------------------
 print_list:
-	addiu $sp, $sp, -16		# Begin print_list
-	sw $s0, 12($sp)
-	sw $s1, 8($sp)
-	sw $s2, 4($sp)
-	sw $ra, 0($sp)
-	move $s0, $a0
-	move $s1, $a1
-	move $s2, $a2
+        addiu $sp, $sp, -16		# Begin print_list
+        sw $s0, 12($sp)
+        sw $s1, 8($sp)
+        sw $s2, 4($sp)
+        sw $ra, 0($sp)
+        move $s0, $a0
+        move $s1, $a1
+        move $s2, $a2
 print_list_loop:
-	beq $s0, $0, print_list_end
-	lw $a0, 0($s0)
-	lw $a1, 4($s0)
-	move $a2, $s2
-	jalr $s1
-	lw $s0, 8($s0)
-	j print_list_loop
+        beq $s0, $0, print_list_end
+        lw $a0, 0($s0)
+        lw $a1, 4($s0)
+        move $a2, $s2
+        jalr $s1
+        lw $s0, 8($s0)
+        j print_list_loop
 print_list_end:
-	lw $s0, 12($sp)
-	lw $s1, 8($sp)
-	lw $s2, 4($sp)
-	lw $ra, 0($sp)
-	addiu $sp, $sp, 16
-	jr $ra			# End print_list	
+        lw $s0, 12($sp)
+        lw $s1, 8($sp)
+        lw $s2, 4($sp)
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 16
+        jr $ra			# End print_list
 
 #------------------------------------------------------------------------------
 # function print_symbol() - DO NOT MODIFY THIS FUNCTION
@@ -148,16 +183,16 @@ print_list_end:
 # Returns: none
 #------------------------------------------------------------------------------
 print_symbol:
-	li $v0, 36			# Begin print_symbol()
-	syscall
-	la $a0, tab
-	li $v0, 4
-	syscall
-	move $a0, $a1
-	syscall
-	la $a0, newline
-	syscall
-	jr $ra			# End print_symbol()
+        li $v0, 36			# Begin print_symbol()
+        syscall
+        la $a0, tab
+        li $v0, 4
+        syscall
+        move $a0, $a1
+        syscall
+        la $a0, newline
+        syscall
+        jr $ra			# End print_symbol()
 
 #------------------------------------------------------------------------------
 # function write_symbol() - DO NOT MODIFY THIS FUNCTION
@@ -171,53 +206,53 @@ print_symbol:
 #
 # Returns: none
 #------------------------------------------------------------------------------
-write_symbol:		
-	addiu $sp, $sp, -20		# Begin write_symbol()
-	sw $s0, 16($sp)
-	sw $s1, 12($sp)
-	sw $a1, 8($sp)
-	sw $a2, 4($sp)
-	sw $ra, 0($sp)
-	
-	la $s0, temp_buf
-	
-	move $a1, $s0
-	jal dec_to_str	# write int
-	move $s1, $v0	
-	
-	addu $a0, $s0, $s1
-	addiu $s1, $s1, 1
-	la $a1, tab
-	li $a2, 1
-	jal strncpy		# write tab
-	
-	lw $a0, 8($sp)
-	jal strlen
-	
-	addu $a0, $s0, $s1
-	addu $s1, $s1, $v0
-	lw $a1, 8($sp)
-	move $a2, $v0
-	jal strncpy		# write string
-	
-	addu $a0, $s0, $s1
-	addiu $s1, $s1, 1
-	la $a1, newline
-	li $a2, 1
-	jal strncpy		# write newline
-	
-	lw $a0, 4($sp)	# file ptr
-	move $a1, $s0	# buffer to write
-	move $a2, $s1	# num chars to write
-	li $v0, 15
-	syscall		# write to file
-	
-	lw $s0, 16($sp)
-	lw $s1, 12($sp)
-	lw $ra, 0($sp)
-	addiu $sp, $sp, 20
-	jr $ra			# End write_symbol()
-	
+write_symbol:
+        addiu $sp, $sp, -20		# Begin write_symbol()
+        sw $s0, 16($sp)
+        sw $s1, 12($sp)
+        sw $a1, 8($sp)
+        sw $a2, 4($sp)
+        sw $ra, 0($sp)
+
+        la $s0, temp_buf
+
+        move $a1, $s0
+        jal dec_to_str	# write int
+        move $s1, $v0
+
+        addu $a0, $s0, $s1
+        addiu $s1, $s1, 1
+        la $a1, tab
+        li $a2, 1
+        jal strncpy		# write tab
+
+        lw $a0, 8($sp)
+        jal strlen
+
+        addu $a0, $s0, $s1
+        addu $s1, $s1, $v0
+        lw $a1, 8($sp)
+        move $a2, $v0
+        jal strncpy		# write string
+
+        addu $a0, $s0, $s1
+        addiu $s1, $s1, 1
+        la $a1, newline
+        li $a2, 1
+        jal strncpy		# write newline
+
+        lw $a0, 4($sp)	# file ptr
+        move $a1, $s0	# buffer to write
+        move $a2, $s1	# num chars to write
+        li $v0, 15
+        syscall		# write to file
+
+        lw $s0, 16($sp)
+        lw $s1, 12($sp)
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 20
+        jr $ra			# End write_symbol()
+
 #------------------------------------------------------------------------------
 # function new_node() - DO NOT MODIFY THIS FUNCTION
 #------------------------------------------------------------------------------
@@ -225,11 +260,11 @@ write_symbol:
 # Arguments: none
 # Returns: pointer to a SymbolList node
 #------------------------------------------------------------------------------
-new_node:	
-	li $a0, 12			# Begin new_node()
-	li $v0, 9
-	syscall
-	jr $ra			# End new_node()
-	
-.data 
+new_node:
+        li $a0, 12			# Begin new_node()
+        li $v0, 9
+        syscall
+        jr $ra			# End new_node()
+
+.data
 temp_buf:	.space 1024
