d3b341feac0d27fc15f08a57acb9849066cb80b1
Dominik Stodolny <dominik@stodolny.org>
2020-05-14 18:15:03 +0100

    fix add_to_list

--- proj1/linker-src/symbol_list.s
+++ proj1/linker-src/symbol_list.s
@@ -93,20 +93,30 @@ addr_for_symbol_done:
 # Returns: the new list
 #------------------------------------------------------------------------------
 add_to_list:
-        addiu $sp, $sp, -8
+        addiu $sp, $sp, -16
         sw $ra, 0($sp)
-        sw $a0, 4($sp)
+        sw $s0, 4($sp)
+        sw $s1, 8($sp)
+        sw $s2, 12($sp)
+
+        move $s0, $a0
+        move $s1, $a1
+        move $a0, $a2
+        jal copy_of_str
+        move $s3, $v0
 
         jal new_node
-        lw $a0, 4($sp)
-        sw $a1, 0($v0)
-        sw $a2, 4($v0)
-        sw $a0, 8($v0)
+        sw $s1, 0($v0)
+        sw $s3, 4($v0)
+        sw $s0, 8($v0)
 
-        lw $a0, 4($sp)
+        lw $s2, 12($sp)
+        lw $s1, 8($sp)
+        lw $s0, 4($sp)
         lw $ra, 0($sp)
-        addiu $sp, $sp, 8
+        addiu $sp, $sp, 16
         jr $ra
+
 
 ###############################################################################
 #                 DO NOT MODIFY ANYTHING BELOW THIS POINT
